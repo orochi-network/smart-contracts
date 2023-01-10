166f2776082af82f2f3f66508cfa09ce778d60b7
Chiro Hiro <chiro8x@gmail.com>
2023-01-10 14:32:29 +0700

    release Orosign V1 smart contracts

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# orosign-contracts
+# Orosign Smart Contracts
+
 Smart Contracts of Orosign project
+
+## Orosign on BNB testnet
+
+```text
+> Deploying: test/BigO                            0xdeeAdC11B67d0cDcBF2869E74563d84dd21B4631
+> Deploying: OrosignV1/OrosignV1                  0x405f8f3dD47F2C0f0eB7016925dAac45d2b6B2ed
+> Deploying: OrosignV1/OrosignMasterV1            0x078fD9549d5EFB0d4133b5DE879A72ad44FC535b
+[Report for network: bnbtest] --------------------------------------------------------
+        test/BigO:                                       0xdeeAdC11B67d0cDcBF2869E74563d84dd21B4631
+        OrosignV1/OrosignV1:                             0x405f8f3dD47F2C0f0eB7016925dAac45d2b6B2ed
+        OrosignV1/OrosignMasterV1:                       0x078fD9549d5EFB0d4133b5DE879A72ad44FC535b
+[End of Report for network: bnbtest] -------------------------------------------------
+Deployer: 0xD62f6bCc528AAb6D21d63102bE4Fdf60C83dCDE5
+```
+
+## Deployed Smart Contracts
+
+- [0xdeeAdC11B67d0cDcBF2869E74563d84dd21B4631](https://testnet.bscscan.com/address/0xdeeadc11b67d0cdcbf2869e74563d84dd21b4631#code) BigO Test Token
+- [0x405f8f3dD47F2C0f0eB7016925dAac45d2b6B2ed](https://testnet.bscscan.com/address/0x405f8f3dD47F2C0f0eB7016925dAac45d2b6B2ed#code) OrosignV1 Implement
+- [0x078fD9549d5EFB0d4133b5DE879A72ad44FC535b](https://testnet.bscscan.com/address/0x078fD9549d5EFB0d4133b5DE879A72ad44FC535b#code) OrosignMasterV1
---
+++ hardhat.config.ts
@@ -0,0 +1,62 @@
+import fs from 'fs';
+import { HardhatUserConfig } from 'hardhat/types';
+import { env } from './env';
+import '@nomicfoundation/hardhat-toolbox';
+
+if (fs.existsSync('./typechain-types')) {
+  const dir = fs.opendirSync(`${__dirname}/tasks`);
+  for (let entry = dir.readSync(); entry !== null; entry = dir.readSync()) {
+    if (entry.name.toLowerCase().includes('.ts')) {
+      // eslint-disable-next-line import/no-dynamic-require
+      require(`./tasks/${entry.name.replace(/\.ts$/gi, '')}`);
+    }
+  }
+}
+
+const compilers = ['0.8.7'].map((item: string) => ({
+  version: item,
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+}));
+
+const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    bnbtest: {
+      url: env.OROCHI_RPC,
+      chainId: 97,
+      accounts: {
+        mnemonic: env.OROCHI_MNEMONIC,
+        path: "m/44'/60'/0'/0",
+      },
+    },
+    // Hard hat network
+    hardhat: {
+      chainId: 911,
+      hardfork: 'london',
+      blockGasLimit: 30000000,
+      initialBaseFeePerGas: 0,
+      gas: 25000000,
+      accounts: {
+        mnemonic: env.OROCHI_MNEMONIC,
+        path: "m/44'/60'/0'/0",
+      },
+      // Are we going to forking mainnet for testing?
+      forking: env.OROCHI_FORK
+        ? {
+            url: env.OROCHI_RPC,
+            enabled: true,
+          }
+        : undefined,
+    },
+  },
+  solidity: {
+    compilers,
+  },
+};
+
+export default config;
