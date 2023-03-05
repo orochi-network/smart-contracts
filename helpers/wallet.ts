15e3b8b9e199153352c44b30472c53578bdb6fb9
Chiro Hiro <chiro8x@gmail.com>
2023-03-05 20:28:11 +0700

    Make fee look easier to understand

--- helpers/wallet.ts
+++ helpers/wallet.ts
@@ -3,6 +3,7 @@ import '@nomiclabs/hardhat-ethers';
 import { BigNumber, ethers } from 'ethers';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { env } from '../env';
+import { NATIVE_UNIT } from './const';
 
 export async function getWallet(hre: HardhatRuntimeEnvironment): Promise<ethers.Wallet> {
   let { chainId, name } = await hre.ethers.provider.getNetwork();
@@ -17,21 +18,26 @@ export function getFee(chainId: number): BigNumber {
     case 10:
     case 42161:
       // Ethereum and its side chains
-      return BigNumber.from('1000000000000000');
+      // 0.001 ETH
+      return BigNumber.from(NATIVE_UNIT).div(1000);
     case 56:
       // BNB Chain
-      return BigNumber.from('5000000000000000');
+      // 0.005 BNB
+      return BigNumber.from(NATIVE_UNIT).div(200);
     case 250:
       // Fantom Chain
-      return BigNumber.from('3000000000000000000');
+      // 3 FTM
+      return BigNumber.from(NATIVE_UNIT).mul(3);
     case 66:
       // OKX Chain
-      return BigNumber.from('50000000000000000');
+      // 0.05 OKT
+      return BigNumber.from(NATIVE_UNIT).div(20);
     case 97:
       // BNB Testnet
-      return BigNumber.from('1000');
+      return BigNumber.from(NATIVE_UNIT).div(1000000000);
     default:
       // Other chain
-      return BigNumber.from('1000000000000000000');
+      // 1
+      return BigNumber.from(NATIVE_UNIT);
   }
 }
