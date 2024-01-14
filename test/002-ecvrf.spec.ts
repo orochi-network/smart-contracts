0dea0cac98c56eb55963edb9a3bb80bebbb8afe1
Chiro Hiro <chiro8x@gmail.com>
2024-01-15 00:38:59 +0700

    Add new test case for latest code

--- test/002-ecvrf.spec.ts
+++ test/002-ecvrf.spec.ts
@@ -11,6 +11,9 @@ let deployer;
 
 const pk =
   '0446b01e9550b56f3655dbca90cfe6b31dec3ff137f825561c563444096803531e9d4f6e8329d300483a919b63843174f1fca692fc6d2c07b985f72386e4edc846';
+const pk2 =
+  '044c2abdb2dc3b03b927fab67aabf80af9d9d0a68681ea0647a455cce2ffd6ebd0bd08e6456ea6807d6f63735e108bd6b1188e2ee7caa0569153b98bd844b3f3a7';
+
 const record = {
   network: 56,
   epoch: 21,
@@ -29,7 +32,42 @@ const record = {
   created_date: '2022-12-25 09:35:47',
 };
 
-const optimus = ((e) => {
+const epochs = [
+  {
+    epoch: 0,
+    alpha: '5ed8efd8419778d8feade85c1d4186ccdb724283b2219572d08fbfe9c1741bfb',
+    gamma:
+      '21cf37b93c19fb22897190988c6d1b95752866631093b3740a1aaedcc9d073deba7f464d289245a414e9fc8252adc6644937c85b59b9a3b5f1468ff103a7ba07',
+    c: 'a05c149d19f8e422a6bec36c51ccaac36fc29cfafd67bd5c4298c7f4541a71f6',
+    s: '9afdc6529547bc2ed780bf9e33848c0ab13670fbbc9fc776320d0b7b41264f44',
+    y: '621c86224c5b8a5ece55b8c55e8d07af317bc9a2f0be84816a5ef2c813afd1d1',
+    witnessAddress: '4aece97d9b193de2517612039f83f4f806a964ed',
+    witnessGamma:
+      '3c2aba780c8bbd9f3a6269359d768107acc154da1b4b1f1cb8e5b8db9637e09b379eede4da9ec1f3c5a13a71460b86c8c9b8764a13f2857858094e7c91b57083',
+    witnessHash:
+      '67810201d9578802db955655bd6f76f11f0cf714a247f4b7dc3dcfaed02c02d37fe43c91d821273b018d3c45b1df9ee4c993e63c5780d6885be6b5e42e47d9a0',
+    inverseZ: '6da4df6f1ca542ad88e8096eed8d059cc4b2ab4cdec4abb06fb2717e0d51fe29',
+    signatureProof:
+      'a43d3e65eed901055dc142cdcd13ad524b9e622bb2f24a53621d19deb95594b648c1ba43e2bbc1212f07f7961a85d9f9fa96c13b38251e3c0799c97eaee1dce51b0000000000000000000000080f53d56bce68dc724687a1c89eea793fd6778881621c86224c5b8a5ece55b8c55e8d07af317bc9a2f0be84816a5ef2c813afd1d1',
+    createdDate: '2024-01-14 17:22:23',
+  },
+];
+
+const converOrandV1Record = (e: any) => {
+  return {
+    pk: [`0x${pk2.substring(2, 66)}`, `0x${pk2.substring(66, 130)}`],
+    seed: `0x${e.alpha}`,
+    gamma: [`0x${e.gamma.substring(0, 64)}`, `0x${e.gamma.substring(64, 128)}`],
+    c: `0x${e.c}`,
+    s: `0x${e.s}`,
+    uWitness: `0x${e.witnessAddress}`,
+    cGammaWitness: [`0x${e.witnessGamma.substring(0, 64)}`, `0x${e.witnessGamma.substring(64, 128)}`],
+    sHashWitness: [`0x${e.witnessHash.substring(0, 64)}`, `0x${e.witnessHash.substring(64, 128)}`],
+    zInv: `0x${e.inverseZ}`,
+  };
+};
+
+const converOrandRecord = (e: any) => {
   return {
     pk: [`0x${pk.substring(2, 66)}`, `0x${pk.substring(66, 130)}`],
     seed: `0x${e.alpha}`,
@@ -41,7 +79,9 @@ const optimus = ((e) => {
     sHashWitness: [`0x${e.witness_hash.substring(0, 64)}`, `0x${e.witness_hash.substring(64, 128)}`],
     zInv: `0x${e.inverse_z}`,
   };
-})(record);
+};
+
+const optimus = converOrandRecord(record);
 
 describe('OrandECVRF', function () {
   it('Orochi ECVRF must be deployed correctly', async () => {
@@ -101,5 +141,15 @@ describe('OrandECVRF', function () {
   it('should able to verify the proof', async () => {
     const output = await orochiECVRF.verifyProof(optimus as any, optimus.seed);
     console.log(`\tverifyProof() -> output: ${await output.toHexString()}`);
+  });
+
+  it('should able to verify multiple epochs', async () => {
+    for (let i = 0; i < epochs.length; i += 1) {
+      const optimus = converOrandV1Record(epochs[i]);
+      console.log(optimus);
+      const output = await orochiECVRF.verifyProof(optimus as any, optimus.seed);
+      console.log(`\tverifyProof() -> output: ${await output.toHexString()}`);
+      console.log(`\t y -> output: ${epochs[i].y}`);
+    }
   });
 });
