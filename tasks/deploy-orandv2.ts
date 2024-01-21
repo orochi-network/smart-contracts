79fcaf76ab9e965a9c978b0c7e8a4f5e09cd8aca
Chiro Hiro <chiro8x@gmail.com>
2024-01-21 13:53:50 +0700

    Update deployment script

--- tasks/deploy-orandv2.ts
+++ tasks/deploy-orandv2.ts
@@ -11,21 +11,21 @@ task('deploy:orandv2', 'Deploy Orand V2 contracts').setAction(
   async (_taskArgs: any, hre: HardhatRuntimeEnvironment) => {
     let pk = env.OROCHI_PUBLIC_KEY.replace(/^0x/gi, '');
     let correspondingAddress = getAddress(`0x${keccak256(`0x${pk.substring(2, 130)}`).substring(26, 66)}`);
-    console.log(`Corresponding address: ${correspondingAddress}`);
 
     const accounts = await hre.ethers.getSigners();
     const deployer: Deployer = Deployer.getInstance(hre).connect(accounts[0]);
     const orandECVRF = await deployer.contractDeploy<OrandECVRFV2>('OrandV2/OrandECVRFV2', []);
-    await deployer.contractDeploy<OrandProviderV2>(
+    const orandProviderV2 = await deployer.contractDeploy<OrandProviderV2>(
       'OrandV2/OrandProviderV2',
       [],
-      // This public key is corresponding to 0x7e9e03a453867a7046B0277f6cD72E1B59f67a0e
       // We going to skip 0x04 -> Pubkey format from libsecp256k1
-      `0x${pk.substring(2, 130)}`,
-      // Operator address
-      correspondingAddress,
+      [`0x${pk.substring(2, 66)}`, `0x${pk.substring(66, 130)}`],
       orandECVRF,
-      NATIVE_UNIT / 100n,
+    );
+
+    console.log(
+      `Corresponding address: ${correspondingAddress} , is valid publicKey?:`,
+      correspondingAddress === (await orandProviderV2.getOperator()),
     );
 
     await deployer.printReport();
