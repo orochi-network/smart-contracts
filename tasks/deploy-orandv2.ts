13ae3f3bd1b3dc1911c5c7ca74393d1d2e1ea006
Chiro Hiro <chiro8x@gmail.com>
2024-01-25 03:23:47 -0800

    Update migration script for A8

--- tasks/deploy-orandv2.ts
+++ tasks/deploy-orandv2.ts
@@ -2,26 +2,39 @@
 import '@nomicfoundation/hardhat-ethers';
 import { task } from 'hardhat/config';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { Deployer, NATIVE_UNIT } from '../helpers';
-import { OrandECVRFV2, OrandProviderV2 } from '../typechain-types';
+import { Deployer } from '../helpers';
+import { DiceGame, OrandECVRFV2, OrandProviderV2 } from '../typechain-types';
 import { env } from '../env';
 import { getAddress, keccak256 } from 'ethers';
 
+const affineToNumberish = (affine: string): [string, string] => {
+  const aff = affine.trim().replace(/^0x/gi, '').padStart(128, '0');
+  return [`0x${aff.substring(0, 64)}`, `0x${aff.substring(64, 128)}`];
+};
+
+const publicKeyToNumberish = (pubkey: string): [string, string] => {
+  const aff = pubkey.trim().replace(/^0x/gi, '').padStart(130, '0').substring(2, 130);
+  return affineToNumberish(aff);
+};
+
 task('deploy:orandv2', 'Deploy Orand V2 contracts').setAction(
   async (_taskArgs: any, hre: HardhatRuntimeEnvironment) => {
-    let pk = env.OROCHI_PUBLIC_KEY.replace(/^0x/gi, '');
+    let pk = env.OROCHI_PUBLIC_KEY.replace(/^0x/gi, '').trim();
     let correspondingAddress = getAddress(`0x${keccak256(`0x${pk.substring(2, 130)}`).substring(26, 66)}`);
 
     const accounts = await hre.ethers.getSigners();
     const deployer: Deployer = Deployer.getInstance(hre).connect(accounts[0]);
     const orandECVRF = await deployer.contractDeploy<OrandECVRFV2>('OrandV2/OrandECVRFV2', []);
+
     const orandProviderV2 = await deployer.contractDeploy<OrandProviderV2>(
       'OrandV2/OrandProviderV2',
       [],
       // We going to skip 0x04 -> Pubkey format from libsecp256k1
-      [`0x${pk.substring(2, 66)}`, `0x${pk.substring(66, 130)}`],
+      publicKeyToNumberish(pk),
       orandECVRF,
+      100,
     );
+    await deployer.contractDeploy<DiceGame>('examples/DiceGame', [], orandProviderV2);
 
     console.log(
       `Corresponding address: ${correspondingAddress} , is valid publicKey?:`,
