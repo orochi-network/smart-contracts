5114168b22cdc0a36aa520db0823e195a8550286
Chiro Hiro <chiro8x@gmail.com>
2024-01-26 22:10:23 -0800

    Update new dual proof

--- contracts/orand-v2/interfaces/IOrandManagementV2.sol
+++ contracts/orand-v2/interfaces/IOrandManagementV2.sol
@@ -5,6 +5,6 @@ interface IOrandManagementV2 {
   // Get public key
   function getPublicKey() external view returns (uint256[2] memory pubKey);
 
-  // Get operator address
-  function getOperator() external view returns (address operator);
+  // Get digest of corresponding public key
+  function getPublicKeyDigest() external view returns (bytes32 operator);
 }
