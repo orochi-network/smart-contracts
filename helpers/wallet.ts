01e1b54c006fb0c78480d2060bf14fdf0a824eb7
Chiro Hiro <chiro8x@gmail.com>
2024-05-07 18:06:38 +0700

    Update task and clean up yarn lock

--- helpers/wallet.ts
+++ helpers/wallet.ts
@@ -1,42 +1,19 @@
 /* eslint-disable no-await-in-loop */
 import '@nomicfoundation/hardhat-ethers';
-import { ethers } from 'ethers';
+import { Wallet, ethers } from 'ethers';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { env } from '../env';
-import { NATIVE_UNIT } from './const';
 
-export async function getWallet(hre: HardhatRuntimeEnvironment): Promise<ethers.HDNodeWallet> {
-  let { chainId, name } = await hre.ethers.provider.getNetwork();
-  console.log(`Network: ${name} ChainID: ${chainId} Path: m/44'/60'/0'/0/${chainId}`);
-  return hre.ethers.Wallet.fromPhrase(env.OROCHI_MNEMONIC, hre.ethers.provider).derivePath(`m/44'/60'/0'/0/${chainId}`);
-}
-
-export function getFee(chainId: bigint): bigint {
-  switch (chainId) {
-    case 1n:
-    case 10n:
-    case 42161n:
-      // Ethereum and its side chains
-      // 0.001 ETH
-      return NATIVE_UNIT / 1000n;
-    case 56n:
-      // BNB Chain
-      // 0.005 BNB
-      return NATIVE_UNIT / 200n;
-    case 250n:
-      // Fantom Chain
-      // 3 FTM
-      return NATIVE_UNIT / 3n;
-    case 66n:
-      // OKX Chain
-      // 0.05 OKT
-      return NATIVE_UNIT / 20n;
-    case 97n:
-      // BNB Testnet
-      return NATIVE_UNIT / 1000000000n;
-    default:
-      // Other chain
-      // 1
-      return NATIVE_UNIT;
+export async function getWallet(hre: HardhatRuntimeEnvironment, chainId: bigint): Promise<ethers.HDNodeWallet> {
+  if (chainId === 911n) {
+    const wallet = (await hre.ethers.getSigners())[0];
+    console.log(
+      `ChainID: ${chainId.toString().padEnd(16, ' ')} Address: ${wallet.address} Path: m/44'/60'/0'/0/${chainId}`,
+    );
+    return wallet as any;
+  } else {
+    const wallet = Wallet.fromPhrase(env.OROCHI_MNEMONIC.trim(), hre.ethers.provider).deriveChild(chainId);
+    console.log(`ChainID: ${chainId.toString().padEnd(16, ' ')} Address: ${wallet.address} Path: ${wallet.path}`);
+    return wallet;
   }
 }
