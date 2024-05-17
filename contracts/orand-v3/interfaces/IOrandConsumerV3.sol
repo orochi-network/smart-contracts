31323ba4e7a8ce1657216d14c687d8344dfa0b1e
Chiro Hiro <chiro8x@gmail.com>
2024-05-17 14:17:15 +0700

    Update interface of OrandProviderV3's consumer

--- contracts/orand-v3/interfaces/IOrandConsumerV3.sol
+++ contracts/orand-v3/interfaces/IOrandConsumerV3.sol
@@ -3,10 +3,19 @@ pragma solidity ^0.8.0;
 
 error InvalidProvider();
 
+/**
+* @dev IOrandConsumerV3 must be implemented for all service that use Orand
+*/
 interface IOrandConsumerV3 {
-  // Consume the verifiable randomness from Orand provider
-  // Return false if you want to stop batching
+  /**
+  * Consume the verifiable randomness from Orand provider
+  * @param randomness Randomness value
+  * @return return false if you want to stop batching otherwise return true
+  */
   function consumeRandomness(uint256 randomness) external returns (bool);
-  // Return true if all requests are fulfilled otherwise return false
+  /**
+  * Check the fulfill status of randomness batching
+  * @return true if all requests are fulfilled otherwise return false
+  */
   function isFulfilled() external returns (bool);
 }
