7b22e6aa20d9b1d48ecc066bb8daff4d3c4ea1cb
dqtkien <dqtkien@gmail.com>
2024-05-21 15:53:49 +0700

    Modify contract name

--- contracts/test/TestERC721.sol
+++ contracts/test/TestERC721.sol
@@ -4,6 +4,6 @@ pragma solidity 0.8.19;
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 
 // An ambition is hiding in the bush
-contract TestNFT is ERC721 {
+contract TestERC721 is ERC721 {
   constructor() ERC721('TestBigO', 'TestO') {}
 }
