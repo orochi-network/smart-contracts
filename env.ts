ce9381979763083d8055e979f4c854cdd3fd5f2c
dqtkien <dqtkien@gmail.com>
2024-06-26 16:24:34 +0700

    Implement on/ off logger feature

--- env.ts
+++ env.ts
@@ -17,6 +17,9 @@ const OROCHI_CONFIGURATION = {
   LOCAL_RPC: '',
   RESULT_PATH: './output/result.json',
   OROCHI_MNEMONIC: '',
+  X_LAYER_API_KEY: '',
+  BSC_API_KEY: '',
+  VERBOSE_SILENT: 'false',
 };
 
 export type TEnvironment = typeof OROCHI_CONFIGURATION;
@@ -25,29 +28,47 @@ if (!fs.existsSync(`${__dirname}/.env`)) {
   throw new Error('.env file not found');
 }
 
-function load(): any {
+function initEnv() {
   const keys = Object.keys(OROCHI_CONFIGURATION);
   const cleaned: any = {};
   for (let i = 0; i < keys.length; i += 1) {
     const k: keyof TEnvironment = keys[i] as keyof TEnvironment;
     const v = process.env[k] || OROCHI_CONFIGURATION[k];
-    if (['USE_ZKSOLC', 'OROCHI_FORK'].includes(k)) {
+    if (['USE_ZKSOLC', 'OROCHI_FORK', 'VERBOSE_SILENT'].includes(k)) {
       cleaned[k] = typeof v === 'boolean' ? v : v.trim().toLowerCase() === 'true';
     } else {
       cleaned[k] = v.trim();
-    }
-    if (typeof process.env[k] !== 'undefined') {
-      delete process.env[k];
     }
   }
   if (!isAddress(cleaned['OROCHI_OWNER'])) {
     throw new Error('Invalid owner address');
   }
-  console.log('Owner is:', cleaned['OROCHI_OWNER']);
+  return cleaned;
+}
+
+const rawEnv: TEnvironment = initEnv();
+export const logger = rawEnv.VERBOSE_SILENT
+  ? (new Proxy(
+      {},
+      {
+        get() {
+          return () => {};
+        },
+      },
+    ) as Console)
+  : new Proxy(console, {
+      get(target, prop, receiver) {
+        return Reflect.get(target, prop, receiver);
+      },
+    });
+
+function load(): any {
+  const cleaned = initEnv();
+  logger.log('Owner is:', cleaned['OROCHI_OWNER']);
   if (!cleaned['OROCHI_OPERATOR'].split(',').every((e: string) => isAddress(e))) {
-    console.log('Error: Can not load operator address at initial');
+    logger.log('Error: Can not load operator address at initial');
   } else {
-    console.log('Operators are:', cleaned['OROCHI_OPERATOR']);
+    logger.log('Operators are:', cleaned['OROCHI_OPERATOR']);
   }
   if (!HexString.isHexStringLike(cleaned['OROCHI_PUBLIC_KEY']) || cleaned['OROCHI_PUBLIC_KEY'].length !== 130) {
     throw new Error('Invalid public key');
@@ -59,11 +80,18 @@ function load(): any {
       `Unexpected corresponding address, want: ${cleaned['OROCHI_CORRESPONDING_ADDRESS']} received: ${correspondingAddress}`,
     );
   }
-  console.log('Public key:', cleaned['OROCHI_PUBLIC_KEY']);
-  console.log('Corresponding address:', cleaned['OROCHI_CORRESPONDING_ADDRESS']);
+  logger.log('Public key:', cleaned['OROCHI_PUBLIC_KEY']);
+  logger.log('Corresponding address:', cleaned['OROCHI_CORRESPONDING_ADDRESS']);
 
   cleaned.OROCHI_FORK = cleaned.OROCHI_FORK || false;
   cleaned.LOCAL_RPC = cleaned.LOCAL_RPC || 'http://127.0.0.1:8545';
+  const keys = Object.keys(OROCHI_CONFIGURATION);
+  for (let i = 0; i < keys.length; i += 1) {
+    const k: keyof TEnvironment = keys[i] as keyof TEnvironment;
+    if (typeof process.env[k] !== 'undefined') {
+      delete process.env[k];
+    }
+  }
   return cleaned;
 }
 
