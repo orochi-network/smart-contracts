85839fb31dc3be46a1897e6df741cdad5763302d
dqtkien <dqtkien@gmail.com>
2024-08-30 23:14:42 +0700

    Add deployment script

--- tasks/deploy-xoro.ts
+++ tasks/deploy-xoro.ts
@@ -1,28 +1,30 @@
-/* eslint-disable no-await-in-loop */
 import '@nomicfoundation/hardhat-ethers';
 import { task } from 'hardhat/config';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { env } from '../env';
 import { Deployer } from '../helpers';
 import { getWallet } from '../helpers/wallet';
-import { XOroV2 } from '../typechain-types';
+import { XORO } from '../typechain-types';
+import { isAddress } from 'ethers';
 
-task('deploy:x-oro-v2', 'Deploy Soul bound token X-OROV2').setAction(async (_, hre: HardhatRuntimeEnvironment) => {
+const TOKEN_NAME = 'ORC [Beta Token]';
+const TOKEN_SYMBOL = 'XORO';
+
+task('deploy:xoro', 'Deploy XORO token').setAction(async (_, hre: HardhatRuntimeEnvironment) => {
   const { chainId } = await hre.ethers.provider.getNetwork();
   const account = await getWallet(hre, chainId);
   const deployer: Deployer = Deployer.getInstance(hre).connect(account);
   const OPERATORS = env.OROCHI_OPERATOR.split(',').map((op) => op.trim());
+  for (let i = 0; i < OPERATORS.length; i += 1) {
+    if (!isAddress(OPERATORS[i])) {
+      throw new Error(`Invalid operator address ${i}: ${OPERATORS[i]}`);
+    }
+    console.log(`Operator [${i}]:`, OPERATORS[i]);
+  }
   if (!env.OROCHI_METADATA_URL) {
     throw new Error('Invalid orochi metadata uri');
   }
 
-  await deployer.contractDeploy<XOroV2>(
-    'orochi/XOroV2',
-    [],
-    env.OROCHI_METADATA_URL,
-    OPERATORS.map((item) => item),
-  );
+  await deployer.contractDeploy<XORO>('token/XORO', [], TOKEN_NAME, TOKEN_SYMBOL, OPERATORS);
   await deployer.printReport();
 });
-
-export default {};
