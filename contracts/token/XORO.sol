063af8dccc867b5692d144b4adf3889253fcf03f
dqtkien <dqtkien@gmail.com>
2024-08-30 17:43:26 +0700

    Add XORO token in smart contracts

--- contracts/token/XORO.sol
+++ contracts/token/XORO.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.19;
 
-import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '../libraries/Operatable.sol';
 
-contract XOroV2 is ERC1155, Ownable, Operatable {
+contract XORO is ERC20, Ownable, Operatable {
   // Error: Access denied
   error AccessDenied();
   // Disable token transfer
@@ -15,7 +15,7 @@ contract XOroV2 is ERC1155, Ownable, Operatable {
   }
 
   // Constructor
-  constructor(string memory uri, address[] memory operatorList) ERC1155(uri) {
+  constructor(string memory name_, string memory symbol_, address[] memory operatorList) ERC20(name_, symbol_) {
     for (uint256 i = 0; i < operatorList.length; i += 1) {
       _addOperator(operatorList[i]);
     }
@@ -40,49 +40,43 @@ contract XOroV2 is ERC1155, Ownable, Operatable {
     return _removeOperator(oldOperator);
   }
 
-  // Set token metadata uri
-  function setURI(string memory uri) external onlyOwner {
-    _setURI(uri);
-  }
-
   //====================[  Operator  ]====================
 
-  // Mint token with tokenId and packed data
-  function batchMint(uint256 tokenId, uint256[] calldata packedData) external onlyOperator {
+  // Mint token in packed data
+  function batchMint(uint256[] calldata packedData) external onlyOperator {
     for (uint i = 0; i < packedData.length; i += 1) {
       (uint96 amount, address to) = _unpack(packedData[i]);
-      _mint(to, tokenId, amount, '');
+      _mint(to, amount);
     }
   }
 
-  // Burn token with tokenId and packed data
-  function batchBurn(uint256 tokenId, uint256[] calldata packedData) external onlyOperator {
+  // Burn token in packed data
+  function batchBurn(uint256[] calldata packedData) external onlyOperator {
     for (uint i = 0; i < packedData.length; i += 1) {
       (uint96 amount, address from) = _unpack(packedData[i]);
-      _burn(from, tokenId, amount);
+      _burn(from, amount);
     }
   }
 
   //====================[  Public  ]====================
 
-  //  Disable safeTransferFrom
-  function safeTransferFrom(
-    address from,
-    address to,
-    uint256 id,
-    uint256 amount,
-    bytes memory data
-  ) public override accessDenied {}
+  //  Disable transfer
+  function transfer(address to, uint256 amount) public override accessDenied returns (bool) {}
 
-  //  Disable safeBatchTransferFrom
-  function safeBatchTransferFrom(
-    address from,
-    address to,
-    uint256[] memory ids,
-    uint256[] memory amounts,
-    bytes memory data
-  ) public override accessDenied {}
+  //  Disable approve
+  function approve(address to, uint256 amount) public override accessDenied returns (bool) {}
 
-  // Disable setApprovalForAll
-  function setApprovalForAll(address operator, bool approved) public virtual override accessDenied {}
+  //  Disable transferFrom
+  function transferFrom(address from, address to, uint256 amount) public override accessDenied returns (bool) {}
+
+  //  Disable increaseAllowance
+  function increaseAllowance(address from, uint256 amount) public override accessDenied returns (bool) {}
+
+  //  Disable decreaseAllowance
+  function decreaseAllowance(address from, uint256 amount) public override accessDenied returns (bool) {}
+
+  // Decimals for this token is 0
+  function decimals() public view virtual override returns (uint8) {
+    return 0;
+  }
 }
--- test/010-soulbound.spec.ts
+++ test/010-soulbound.spec.ts
@@ -1,14 +1,12 @@
 import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers';
-import hre, { ethers } from 'hardhat';
+import hre from 'hardhat';
 import Deployer from '../helpers/deployer';
-import { XOroV2 } from '../typechain-types';
+import { XORO } from '../typechain-types';
 import { expect } from 'chai';
 
-const METADATA_API = 'https://metadata.orochi.network/x-oro-v2/{id}.json';
-const NEW_METADATA_API = 'https://metadata.orochi.network/x-oro-v3/{id}.json';
-const TOKEN_ID = 1n;
-const NEW_TOKEN_ID = 2n;
-const ANOTHER_TOKEN_ID = 3n;
+const TOKEN_NAME = 'ORC [Beta Token]';
+const TOKEN_SYMBOL = 'XORO';
+const MAX_AMOUNT_APPROVAL = Number.MAX_SAFE_INTEGER;
 
 let accounts: SignerWithAddress[];
 let deployerSigner: SignerWithAddress;
@@ -21,17 +19,17 @@ let operator2: SignerWithAddress;
 let operator3: SignerWithAddress;
 let fakeOperator: SignerWithAddress;
 
-let contract: XOroV2;
+let contract: XORO;
 
 const packData = (amount: bigint, address: string): bigint => (amount << 160n) | BigInt(address);
 
-describe('Soulbound token', function () {
-  it('Souldbound token must be deployed correctly', async () => {
+describe.only('XORO token', function () {
+  it('XORO token must be deployed correctly', async () => {
     accounts = await hre.ethers.getSigners();
     [deployerSigner, player01, player02, player03, newOwner, operator1, operator2, fakeOperator, operator3] = accounts;
     const deployer: Deployer = Deployer.getInstance(hre);
     deployer.connect(deployerSigner);
-    contract = await deployer.contractDeploy<XOroV2>('orochi/XOroV2', [], METADATA_API, [
+    contract = await deployer.contractDeploy<XORO>('token/XORO', [], TOKEN_NAME, TOKEN_SYMBOL, [
       operator1.address,
       operator2.address,
     ]);
@@ -56,60 +54,54 @@ describe('Soulbound token', function () {
     ];
 
     expect(await contract.owner()).eq(deployerSigner.address);
+    expect(await contract.decimals()).eq(0n);
     expect(await contract.isOperator(operator1)).eq(true);
     expect(await contract.isOperator(operator2)).eq(true);
     expect(await contract.isOperator(operator3)).eq(false);
-    expect(await contract.uri(TOKEN_ID)).eq(METADATA_API);
-    expect(await contract.uri(NEW_TOKEN_ID)).eq(METADATA_API);
-    expect(await contract.uri(ANOTHER_TOKEN_ID)).eq(METADATA_API);
     for (let i = 0; i < data.length; i += 1) {
       packedData.push(packData(data[i].amount, data[i].to));
     }
-    await contract.connect(operator1).batchMint(TOKEN_ID, [...packedData]);
+    await contract.connect(operator1).batchMint([...packedData]);
   });
 
   it('All player must have their correct token amount', async () => {
-    expect(await contract.balanceOf(player01, TOKEN_ID)).eq(20n);
-    expect(await contract.balanceOf(player02, TOKEN_ID)).eq(1n);
-    expect(await contract.balanceOf(player03, TOKEN_ID)).eq(15n);
+    expect(await contract.balanceOf(player01)).eq(20n);
+    expect(await contract.balanceOf(player02)).eq(1n);
+    expect(await contract.balanceOf(player03)).eq(15n);
   });
 
   it('Only operator can mint token', async () => {
-    await expect(
-      contract.connect(newOwner).batchMint(TOKEN_ID, [packData(100n, player01.address)]),
-    ).to.revertedWithCustomError(contract, 'InvalidOperator');
-    expect(await contract.balanceOf(player01, TOKEN_ID)).eq(20n);
+    await expect(contract.connect(newOwner).batchMint([packData(100n, player01.address)])).to.revertedWithCustomError(
+      contract,
+      'InvalidOperator',
+    );
+    expect(await contract.balanceOf(player01)).eq(20n);
   });
 
-  it("Player 1 can approve operator to act with player's 1 token", async () => {
-    // await contract.connect(player01).setApprovalForAll(operator1, true);
-    await expect(contract.connect(player01).setApprovalForAll(operator1, true)).revertedWithCustomError(
+  it("Player 1 can't approve operator to act with player's 1 token", async () => {
+    await expect(contract.connect(player01).approve(operator1.address, MAX_AMOUNT_APPROVAL)).revertedWithCustomError(
       contract,
       'AccessDenied',
     );
   });
 
   it("Player 1 can't transfer token to player 2", async () => {
-    await expect(
-      contract.connect(player01).safeTransferFrom(player01, player02, TOKEN_ID, '5', ethers.toUtf8Bytes('')),
-    ).to.revertedWithCustomError(contract, 'AccessDenied');
-    expect(await contract.balanceOf(player01, TOKEN_ID)).eq(20n);
-    expect(await contract.balanceOf(player02, TOKEN_ID)).eq(1n);
+    await expect(contract.connect(player01).transfer(player02, '5')).to.revertedWithCustomError(
+      contract,
+      'AccessDenied',
+    );
+    expect(await contract.balanceOf(player01)).eq(20n);
+    expect(await contract.balanceOf(player02)).eq(1n);
   });
 
   it("Contract owner can't transfer token to player 1", async () => {
-    await expect(
-      contract.connect(deployerSigner).safeTransferFrom(deployerSigner, player01, TOKEN_ID, 5n, ethers.toUtf8Bytes('')),
-    ).to.revertedWithCustomError(contract, 'AccessDenied');
-    expect(await contract.balanceOf(player01, TOKEN_ID)).eq(20n);
-    expect(await contract.balanceOf(player02, TOKEN_ID)).eq(1n);
-    expect(await contract.balanceOf(deployerSigner, TOKEN_ID)).eq(20n);
-  });
-
-  it("Player 1 can't batch transfer to player 2", async () => {
-    await expect(
-      contract.connect(player01).safeBatchTransferFrom(player01, player02, [TOKEN_ID], [12n], ethers.toUtf8Bytes('')),
-    ).to.revertedWithCustomError(contract, 'AccessDenied');
+    await expect(contract.connect(deployerSigner).transfer(player01, 5n)).to.revertedWithCustomError(
+      contract,
+      'AccessDenied',
+    );
+    expect(await contract.balanceOf(player01)).eq(20n);
+    expect(await contract.balanceOf(player02)).eq(1n);
+    expect(await contract.balanceOf(deployerSigner)).eq(20n);
   });
 
   it('Current owner can transfer ownership to another owner', async () => {
@@ -129,25 +121,17 @@ describe('Soulbound token', function () {
   });
 
   it("Old operator can't mint token anymore", async () => {
-    await expect(
-      contract.connect(operator2).batchMint(TOKEN_ID, [packData(10n, player01.address)]),
-    ).to.revertedWithCustomError(contract, 'InvalidOperator');
+    await expect(contract.connect(operator2).batchMint([packData(10n, player01.address)])).to.revertedWithCustomError(
+      contract,
+      'InvalidOperator',
+    );
 
-    expect(await contract.balanceOf(player01, TOKEN_ID)).eq(20n);
-    await contract.connect(operator1).batchMint(TOKEN_ID, [packData(15n, player02.address)]);
+    expect(await contract.balanceOf(player01)).eq(20n);
+    await contract.connect(operator1).batchMint([packData(15n, player02.address)]);
 
     await contract.connect(newOwner).transferOwnership(deployerSigner);
     expect(await contract.owner()).eq(deployerSigner);
-    expect(await contract.balanceOf(player02, TOKEN_ID)).eq(16n);
-  });
-
-  it('Token can call balanceOfBatch correctly', async () => {
-    console.log(await contract.balanceOfBatch([player01, player02, player03], [TOKEN_ID, TOKEN_ID, TOKEN_ID]));
-    expect(await contract.balanceOfBatch([player01, player02, player03], [TOKEN_ID, TOKEN_ID, TOKEN_ID])).to.deep.eq([
-      20n,
-      16n,
-      15n,
-    ]);
+    expect(await contract.balanceOf(player02)).eq(16n);
   });
 
   it('Only operator can run batchMint', async () => {
@@ -161,84 +145,19 @@ describe('Soulbound token', function () {
       const amount = data[i].amount << 160n;
       packedData.push(amount | BigInt(data[i].to));
     }
-    await expect(contract.connect(operator2).batchMint(TOKEN_ID, [...packedData])).to.revertedWithCustomError(
+    await expect(contract.connect(operator2).batchMint([...packedData])).to.revertedWithCustomError(
       contract,
       'InvalidOperator',
     );
-    await contract.connect(operator1).batchMint(TOKEN_ID, [...packedData]);
+    await contract.connect(operator1).batchMint([...packedData]);
 
-    expect(await contract.balanceOf(player01, TOKEN_ID)).eq(22n);
-    expect(await contract.balanceOf(player02, TOKEN_ID)).eq(21n);
-    expect(await contract.balanceOf(player03, TOKEN_ID)).eq(115n);
-  });
-
-  it('Only operator can mint other tokenId', async () => {
-    await expect(
-      contract.connect(operator2).batchMint(NEW_TOKEN_ID, [packData(100n, player01.address)]),
-    ).to.revertedWithCustomError(contract, 'InvalidOperator');
-    await expect(
-      contract.connect(operator2).batchMint(ANOTHER_TOKEN_ID, [packData(100n, player01.address)]),
-    ).to.revertedWithCustomError(contract, 'InvalidOperator');
-
-    await contract.connect(operator1).batchMint(NEW_TOKEN_ID, [packData(100n, player01.address)]);
-
-    await contract.connect(operator3).batchMint(NEW_TOKEN_ID, [packData(1n, player02.address)]);
-
-    expect(
-      await contract.balanceOfBatch([player01, player02, player03], [NEW_TOKEN_ID, NEW_TOKEN_ID, NEW_TOKEN_ID]),
-    ).to.deep.eq([100n, 1n, 0n]);
-
-    expect(await contract.balanceOfBatch([player01, player02, player03], [TOKEN_ID, TOKEN_ID, TOKEN_ID])).to.deep.eq([
-      22n,
-      21n,
-      115n,
-    ]);
-
-    expect(
-      await contract.balanceOfBatch(
-        [player01, player02, player03],
-        [ANOTHER_TOKEN_ID, ANOTHER_TOKEN_ID, ANOTHER_TOKEN_ID],
-      ),
-    ).to.deep.eq([0n, 0n, 0n]);
-  });
-
-  it('Only operator can run batch mint and it should works correctly with another tokenId', async () => {
-    const packedData = [];
-    const data = [
-      { amount: 4n, to: player01.address },
-      { amount: 5n, to: player02.address },
-      { amount: 10n, to: player03.address },
-    ];
-    for (let i = 0; i < data.length; i += 1) {
-      packedData.push(packData(data[i].amount, data[i].to));
-    }
-    await expect(contract.connect(operator2).batchMint(NEW_TOKEN_ID, [...packedData])).to.revertedWithCustomError(
-      contract,
-      'InvalidOperator',
-    );
-    await contract.connect(operator1).batchMint(NEW_TOKEN_ID, [...packedData]);
-    expect(
-      await contract.balanceOfBatch([player01, player02, player03], [NEW_TOKEN_ID, NEW_TOKEN_ID, NEW_TOKEN_ID]),
-    ).to.deep.eq([104n, 6n, 10n]);
-    expect(
-      await contract.balanceOfBatch(
-        [player01, player02, player03],
-        [ANOTHER_TOKEN_ID, ANOTHER_TOKEN_ID, ANOTHER_TOKEN_ID],
-      ),
-    ).to.deep.eq([0n, 0n, 0n]);
+    expect(await contract.balanceOf(player01)).eq(22n);
+    expect(await contract.balanceOf(player02)).eq(21n);
+    expect(await contract.balanceOf(player03)).eq(115n);
   });
 
   it('Operator can burn token', async () => {
-    await contract.connect(operator1).batchBurn(TOKEN_ID, [packData(12n, player01.address)]);
-    await contract.connect(operator3).batchBurn(NEW_TOKEN_ID, [packData(1n, player02.address)]);
-    expect(await contract.balanceOf(player01, TOKEN_ID)).to.eq(10n);
-    expect(await contract.balanceOf(player02, NEW_TOKEN_ID)).to.eq(5n);
-  });
-
-  it('Only owner can set new metadata url', async () => {
-    await contract.connect(deployerSigner).setURI(NEW_METADATA_API);
-    expect(await contract.uri(TOKEN_ID)).eq(NEW_METADATA_API);
-    await expect(contract.connect(operator1).setURI(METADATA_API)).to.revertedWith('Ownable: caller is not the owner');
-    await expect(contract.connect(newOwner).setURI(METADATA_API)).to.revertedWith('Ownable: caller is not the owner');
+    await contract.connect(operator1).batchBurn([packData(12n, player01.address)]);
+    expect(await contract.balanceOf(player01)).to.eq(10n);
   });
 });
