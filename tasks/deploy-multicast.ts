688aa140dc9305e4f4422d11410ec68daeead8b3
dqtkien <dqtkien@gmail.com>
2024-09-09 15:22:30 +0700

    Modify multicast deployment script with new wallet creation method

--- tasks/deploy-multicast.ts
+++ tasks/deploy-multicast.ts
@@ -4,12 +4,14 @@ import { task } from 'hardhat/config';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { Deployer } from '../helpers';
 import { Multicast } from '../typechain-types';
+import { getWallet } from '../helpers/wallet';
 
 task('deploy:multicast', 'Deploy Multicast contracts').setAction(
   async (_taskArgs: any, hre: HardhatRuntimeEnvironment) => {
-    const accounts = await hre.ethers.getSigners();
+    const { chainId } = await hre.ethers.provider.getNetwork();
+    const account = await getWallet(hre, chainId);
     //0x3ECb21f2c6A5a57C57634036777730bb6E87F281
-    const deployer: Deployer = Deployer.getInstance(hre).connect(accounts[0]);
+    const deployer: Deployer = Deployer.getInstance(hre).connect(account);
     await deployer.contractDeploy<Multicast>('orochi/Multicast', []);
     await deployer.printReport();
   },
