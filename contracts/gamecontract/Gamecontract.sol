047ee94839d101303463cca70432d39d32e8a911
pdquan20 <pdquan20@gmail.com>
2024-12-09 18:18:48 +0700

    Misc: Add note and documnent game contract

--- contracts/gamecontract/Gamecontract.sol
+++ contracts/gamecontract/Gamecontract.sol
@@ -4,18 +4,31 @@ pragma solidity 0.8.19;
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract GameContract is Ownable {
+    // Invalid User
     error InvalidUser();
 
+    // Signer List
     mapping(address => bool) private _signerMap;
 
+    // Signer Total
     uint256 private _signerTotal;
 
+    // Complete daily quest
     event QuestCompleteDaily(address indexed user, bytes32 indexed questName);
+
+    // Complete Social quest
     event QuestCompleteSocial(address indexed user, bytes32 indexed questName);
+
+    // Complete Game quest
     event QuestCompleteGame(address indexed user, bytes32 indexed questName);
+
+    //  Add list Users
     event SignerListAdd(uint256 indexed totalAddedUser, uint256 indexed timestamp);
+
+    // Remove list Users
     event SignerListRemove(uint256 indexed totalAddedUser, uint256 indexed timestamp);
 
+    // We only allow User have been add by owner
     modifier onlyUser() {
         if (!_signerMap[msg.sender]) {
             revert InvalidUser();
@@ -23,6 +36,12 @@ contract GameContract is Ownable {
         _;
     }
 
+
+    /*******************************************************
+    * External section
+    ********************************************************/
+
+    // Add new Users in list
     function signerListAdd(address[] memory signerListToAdd) external onlyOwner {
         for (uint256 i = 0; i < signerListToAdd.length; i += 1) {
             if (!_signerMap[signerListToAdd[i]]) { 
@@ -33,6 +52,7 @@ contract GameContract is Ownable {
         emit SignerListAdd(_signerTotal, block.timestamp);
     }
 
+    // Remove old Users in list
     function signerListRemove(address[] memory listSignerToRemove) external onlyOwner {
         for (uint256 i = 0; i < listSignerToRemove.length; i += 1) {
             if (_signerMap[listSignerToRemove[i]]) { 
@@ -43,18 +63,22 @@ contract GameContract is Ownable {
          emit SignerListRemove(_signerTotal, block.timestamp);
     }
 
+    // submit transaction daily quest
     function questSubmitDaily(bytes32 questName) external onlyUser {
         emit QuestCompleteDaily(msg.sender, questName);
     }
 
+    // submit transaction social quest
     function questSubmitSocial(bytes32 questName) external onlyUser {
         emit QuestCompleteSocial(msg.sender, questName);
     }
 
+    // submit transaction game quest
     function questSubmitGame(bytes32 questName) external onlyUser {
         emit QuestCompleteGame(msg.sender, questName);
     }
 
+    // Check list signer status which have add and which hasn't add
     function signerListCheck(address[] memory signerListToCheck) external view returns (bool[] memory) {
         bool[] memory statusList = new bool[](signerListToCheck.length);
         for (uint256 i = 0; i < signerListToCheck.length; i += 1) {
@@ -63,10 +87,12 @@ contract GameContract is Ownable {
         return statusList; 
     }
 
+    // Check signer status 
     function signerCheck(address signerToCheck) external view returns (bool) {
         return _signerMap[signerToCheck];
     }
 
+    // Total signer has been added
     function signerTotal() external view returns (uint256) {
         return _signerTotal;
     }
