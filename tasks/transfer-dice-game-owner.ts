d2332401c2a84e2dd743e3d2cb7a036b3b1c9fba
pdquan20 <pdquan20@gmail.com>
2025-04-03 10:30:28 +0700

    [WIP]: init erc20

--- tasks/transfer-dice-game-owner.ts
+++ tasks/transfer-dice-game-owner.ts
@@ -4,14 +4,14 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DiceGameV3 } from '../typechain-types';
 import { getWallet } from '../helpers/wallet';
 
-const DICE_GAME_CONTRACT_ADDRESS = '0x57abA949de504cE2c1D55cBB919A5e8233b40c60';
+const DICE_GAME_CONTRACT_ADDRESS = '0xfd3617eF06C02992BD611e74Fc248D027096269D';
 
-task('test:dice-game-v3', 'Test dice game with OrandProviderV3').setAction(
+task('tranferDicegame', 'Test dice game with OrandProviderV3').setAction(
   async (_taskArgs: any, hre: HardhatRuntimeEnvironment) => {
     const { chainId } = await hre.ethers.provider.getNetwork();
     const account = await getWallet(hre, chainId);
     const contract = (await hre.ethers.getContractAt('DiceGameV3', DICE_GAME_CONTRACT_ADDRESS, account)) as DiceGameV3;
-    const tx = await contract.createTestGame();
+    const tx = await contract.transferOwnership("0x73100880b1B6F0De121CAc27C418BF77183e3768");
     console.log('Successful transaction', tx);
   },
 );
